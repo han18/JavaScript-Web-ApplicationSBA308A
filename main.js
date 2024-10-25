b3846f7448be81d71243fce3b6c7f3cd46d6ae68
han18 <han.fadah@gmail.com>
2024-10-25 12:25:43 -0400

    creating a fetch function to hold the data

--- main.js
+++ main.js
@@ -2,4 +2,14 @@
 
 const mainDiv = document.getElementById("main-container");
 
-
+// in this example I used fetch
+async function fetchData() {
+    const url = ' https://restcountries.com/v3.1/all';
+    
+    try {
+        const response = await fetch(url);
+        
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+  
