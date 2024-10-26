e6d29f07a054c3c0166aae2107b23c329dc4f70d
han18 <han.fadah@gmail.com>
2024-10-26 15:20:36 -0400

    fixed the language error

--- main.js
+++ main.js
@@ -35,30 +35,37 @@ const mainDiv = document.getElementById("main-container");
 // storing the url in a variable
 const BASE_URL = "https://restcountries.com/v3.1/all";
 async function getRandomCountry() {
-        try {
-          const response = await axios.get(BASE_URL); // used axios for better request
-          const countries = response.data;
-          console.log(countries);
-    
-          // Select a random country from the list
-          const randomIndex = Math.floor(Math.random() * countries.length);
-          // country hold all the data
-          const country = countries[randomIndex];
-    
-          // displaying country details in the main div container
-          mainDiv.innerHTML = `
+  try {
+    const response = await axios.get(BASE_URL); // used axios for better request
+    const countries = response.data;
+    console.log(countries);
+
+    // Select a random country from the list
+    const randomIndex = Math.floor(Math.random() * countries.length);
+    // variable to hold all the data of the countries
+    const country = countries[randomIndex];
+
+    // getting the languages since the value for each country changes
+    const languages = Object.values(country.languages).join(", ");
+
+    // displaying country details in the main div container
+    let textSpace = " ";
+    mainDiv.innerHTML = `
             <h1>${country.name.common}</h1>
             <img src="${country.flags.png}"/>
             <div id="first-div">
-            <p class="testing"><strong> Capital:</strong> ${ country.capital[0]} </p>
-            <p class="testing"><strong>Capital:</strong> ${country.capital[0]} ${country.capital[0]}</p>
+            <p class="testing" ><strong>Capital:</strong> ${textSpace} ${
+      country.capital[0]
+    } </p>
+            <p class="testing"><strong>Capital:</strong> ${languages}</p>
             </div>
-            <p><strong>Region:</strong>  ${country.continents} <strong>Continent:</strong>  ${country.region}</p>
+            <p><strong>Region:</strong>  ${
+              country.continents
+            } <strong>Continent:</strong>  ${country.region}</p>
             <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
           `;
-        } catch (error) {
-          console.error("Error fetching country data:", error);
-          mainDiv.innerHTML = "<p>Failed to fetch country data.</p>";
-        }
-      }
-    
+  } catch (error) {
+    console.error("Error fetching country data:", error);
+    mainDiv.innerHTML = "<p>Failed to fetch country data.</p>";
+  }
+}
